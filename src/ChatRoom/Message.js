52684d3bf6f33600d3e054bfc8e898dfcc105785
Johnny Huang <jtk155586@gmail.com>
2020-08-20 14:23:37 +0800

    add timestamp

--- src/ChatRoom/Message.js
+++ src/ChatRoom/Message.js
@@ -4,17 +4,20 @@ import { Card, CardContent, Typography } from '@material-ui/core'
 
 const Message = ({ username, message }) => {
   const isUser = username === message.username
+  const date = message.timestamp.toDate()
   return (
-    <div className="message">
-      {!isUser && <label className="message__label">{message.username}</label>}
-      <Card className={`card ${isUser && 'card__user'}`}>
-        <CardContent className="card__content">
-          <Typography>
-            {/* {username !== message.username && `${message.username}: `} */}
-            {message.text}
-          </Typography>
-        </CardContent>
-      </Card>
+    <div className={`message ${isUser && 'message__user'}`}>
+      <div>
+        {!isUser && <label className="message__label">{message.username}</label>}
+        <Card className={`card ${isUser && 'card__user'}`}>
+          <CardContent className="card__content">
+            <Typography style={{ fontWeight: 'bold' }}>{message.text}</Typography>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="message__timestamp">
+        {date.getHours()}:{date.getMinutes()}
+      </div>
     </div>
   )
 }
